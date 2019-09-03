aa87f23a691a74b778d1b5368b7a6ccf32ba911b
Johannes Plunien <joplunien@ebay.com>
2019-09-03 08:18:45 +0200

    Add ViewController

--- SearchControllerExample/AppDelegate.swift
+++ SearchControllerExample/AppDelegate.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
+final class ViewController: UIViewController {
+
+    private let searchController: UISearchController
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Test", style: .plain, target: self, action: #selector(cancelAction(_:)))
+        navigationItem.searchController = searchController
+    }
+
+    @objc private func cancelAction(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Test", message: "👍", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+    @available(*, unavailable) required init?(coder aDecoder: NSCoder) { fatalError() }
+
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     let window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window?.rootViewController = UIViewController()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
         window?.makeKeyAndVisible()
         return true
     }
